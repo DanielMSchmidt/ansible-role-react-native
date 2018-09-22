beaf683f5000e0016cfd37f25e1913483cd398e8
Daniel Schmidt <dschmidt@mesosphere.com>
2018-09-22 17:41:15 +0200

    source bashrc

--- runVagrantTest.sh
+++ runVagrantTest.sh
@@ -3,7 +3,7 @@
 set -ex
 
 # Cleanup previous run
-rm -f ~/vagrant/ansible.cfg ~/vagrant/test.yml
+rm -f ~/vagrant/ansible.cfg ~/vagrant/test.yml ~/.bashrc
 
 cd ~/vagrant/react-native
 
@@ -12,6 +12,7 @@ cd ~/vagrant/react-native
 brew install ansible
 cp test-ansible.cfg ../ansible.cfg
 cp test.yml ../test.yml
+touch ~/.bashrc
 
 # Run provisoining
 cd ~/vagrant
